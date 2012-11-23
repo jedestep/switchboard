e73bc99fb49fbaaf9c03dd7843562e5ac9bd2108
Jed <aje@jhu.edu>
2012-11-23 18:16:41 -0500

    Some fixes related to comment stripping

--- CSound/Lexer.hs
+++ CSound/Lexer.hs
@@ -28,16 +28,14 @@ lexToken = do
 	<|> 	do
 			optional (many wspace)
 			a <- noneOf " \t\v\f\r\n"
-			return (Punct  [a])
+			return (Punct [a])
 
 --representative of a single line			
 lexTokenList :: Parse LexedLine
 lexTokenList = do
 		a <- lexToken
 		try (do
-			optional (many wspace)
 			b <- lexTokenList
-			optional (many wspace)
 			return (a:b)
 		    )
 		    <|> return ([a]++[Terminate])
@@ -45,7 +43,7 @@ lexTokenList = do
 --instead of ignoring comments while lexing, we remove them afterwards
 removeComments :: LexedLine -> LexedLine
 removeComments (l:ls) = case l of
-				Punct ";" -> []
+				Punct ";" -> [Terminate] --since the Terminate is originally after the comments we need to re-terminate the line
 				Punct ":" -> l:(removeComments ls)
 				_	  -> l:(removeComments ls)
 removeComments [] = []
@@ -56,6 +54,10 @@ wspace = char ' ' <|> char '\t'
 --testing and other beauty
 
 --reads a program from a file and lexes it
-fl = "f1 0 4096 1 ; sine wave\n\n;ins strt dur amp(p4) freq(p5)\ni2 0 1 2000 880\ni2 3 1 8000 220\n i2 6 1 32000 55\n\ne"
-getTokenProgram :: String -> LexedProgram
-getTokenProgram a = map removeComments (head $ rights [parse lexedProgram "?" a])
+
+getTokenProgram :: String -> [Token]
+getTokenProgram a = foldr (++) [] (map removeComments (head $ rights [parse lexedProgram "?" a]))
+
+getTokensIO file = do
+	a <- readFile file
+	return $ getTokenProgram a
